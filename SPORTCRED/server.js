a9809d49173dde9a6195a28aa925b1971b300044
Jimmy Yu <jimmyyu82@gmail.com>
2020-10-17 16:31:16 -0400

    Add api route for profile and fix deprecation warning for mongoose connection

--- SPORTCRED/server.js
+++ SPORTCRED/server.js
@@ -2,15 +2,17 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
-const items = require('./routes/api/items')
+const items = require('./routes/api/items');
+const profiles = require('./routes/api/profile');
 const app = express();
 app.use(bodyParser.json());
 
 const db = require('./config/keys').mongoURI;
-mongoose.connect(db)
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(()=> console.log('Mongo Connected...'))
     .catch(err => console.log(err));
 
 app.use('/api/register', items);
+app.use('/api/profile', profiles);
 const port = 5000;
 app.listen(port, () => console.log('Server started at port ${port}'));
