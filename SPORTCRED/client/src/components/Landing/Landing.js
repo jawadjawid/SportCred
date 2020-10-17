c6cec6df11cf9385cbe90a08d9761b6a387d12a9
pi-ukani <piyush.ukani@mail.utoronto.ca>
2020-10-16 22:55:18 -0400

    Attempt to create dialog and signup page

--- SPORTCRED/client/src/components/Landing/Landing.js
+++ SPORTCRED/client/src/components/Landing/Landing.js
@@ -4,12 +4,31 @@ import {style} from './style';
 import {withStyles} from '@material-ui/core';
 import { withRouter } from 'react-router-dom';
 import logo from '../../assets/logo.png';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Signup from './signup';
 
 class Landing extends React.Component {
 
+state = {
+    login: false,
+    signup: false
+};
+openlogin() {
+    this.setState({ login: true });
+}
+opensignup() {
+    this.setState({ signup: true });
+}
+login(state){
+    this.setState({ signup: state });
+}
 render(){
-    const { classes } = this.props;
-     console.log(logo)
+    const { classes,SimpleDialogDemo } = this.props;
+    
+    
     return ( //
         <div className = {classes.Container}>
             <video className = {classes.Video} loop autoPlay muted >
@@ -21,9 +40,26 @@ render(){
                 <div className={classes.SubContent} >
                     <img className={classes.MainLabel} src = {logo} />
                     <div className={classes.NavBar}>
-                    <a className={classes.Login}>Log in</a>
-                    <a className={classes.Login}>Sign up</a>
+                    {/* <a className={classes.Login} onClick={AlertDialog(true)}>Log in</a>
+                    <a className={classes.Login} onClick={AlertDialog(false)}>Sign up</a> */}
                     </div>
+                    <Button onClick={this.openlogin.bind(this)}>Login</Button>
+                    <Button onClick={this.opensignup.bind(this)}>Signup</Button>
+            <Dialog open={this.state.login} >
+                <DialogTitle>Login</DialogTitle>
+                <DialogContent >Start editing to see some magic happen!
+                <a onClick = {
+              () => {
+                this.setState({ login: false })
+              }}>Close</a>
+                </DialogContent>
+            </Dialog>
+            <Dialog open={this.state.signup} >
+                <DialogTitle>Signup</DialogTitle>
+                <DialogContent >Start editing to see some magic happen!
+                    <Signup   component={Signup}></Signup>
+                </DialogContent>
+            </Dialog>
                 </div>
                 <h1 className={classes.Motto}>Hoops to troops</h1>
             </div>
--- SPORTCRED/client/src/components/Landing/style.js
+++ SPORTCRED/client/src/components/Landing/style.js
@@ -54,5 +54,22 @@ export const style = theme => ({
     Motto:{
         color: theme.palette.tertiary.main,
         marginLeft: '37%',
-    }
+    },
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      },
+      avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+      },
+      form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(3),
+      },
+      submit: {
+        margin: theme.spacing(3, 0, 2),
+      },
   })
