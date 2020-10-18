220ee34f73883dc5d1adb9600d57c022b060364e
Jimmy Yu <jimmyyu82@gmail.com>
2020-10-18 03:41:06 -0400

    Implement put requests for profiles. PUT requests give the ability to update profile fields in the database

---
+++ SPORTCRED/routes/api/profile.js
@@ -0,0 +1,204 @@
+const express = require('express');
+const { mongo } = require('mongoose');
+const router = express.Router();
+
+const Profile = require('../../models/profile');
+
+
+router.get('/', (req, res) => {
+    Profile.find()
+        .sort({date: -1})
+        .then(data => res.json(data));
+});
+
+router.post('/', (req, res) => {
+    const profile = new Profile({
+        username: req.body.username,
+        phone: req.body.phone,
+        email: req.body.email,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        DOB: req.body.DOB,
+        picture: req.body.picture,
+        about: req.body.about
+    })
+    profile.save()
+        .then(data => res.json(data));
+});
+
+router.delete('/:username', (req, res, next) => {
+    Profile.deleteOne({ username: req.params.username})
+    .then(() => {
+        res.status(200).json({
+          message: 'Deleted!'
+        });
+      })
+    .catch((error) => {
+        res.status(400).json({
+          error: error
+        });
+      });
+});
+
+router.put('/updatePhone/:username', (req, res, next) => {
+    console.log("Hitting update phone endpt with id " + req.params.username)
+
+    // If phone key is not in JSON body then return 400 status
+    if ((typeof req.body.phone) === 'undefined') {
+        res.status(400).json({
+            error: error
+        })
+    }
+
+    Profile.updateOne({ username: req.params.username }, { phone: req.body.phone })
+        .then(() => {
+            res.status(200).json({
+                message: 'updated successfully'
+            });
+        })
+        .catch(error => {
+            res.status(400).json({
+                error: error
+            });
+        });
+});
+
+router.put('/updateEmail/:username', (req, res, next) => {
+    console.log("Hitting update email endpt with id " + req.params.username)
+
+    // If email key is not in JSON body then return 400 status
+    if ((typeof req.body.email) === 'undefined') {
+        res.status(400).json({
+            error: error
+        })
+    }
+
+    Profile.updateOne({ username: req.params.username }, { email: req.body.email })
+        .then(() => {
+            res.status(200).json({
+                message: 'updated successfully'
+            });
+        })
+        .catch(error => {
+            res.status(400).json({
+                error: error
+            });
+        });
+});
+
+router.put('/updateFirstName/:username', (req, res, next) => {
+    console.log("Hitting update first name endpt with id " + req.params.username)
+
+    // If firstName key is not in JSON body then return 400 status
+    if ((typeof req.body.firstName) === 'undefined') {
+        res.status(400).json({
+            error: error
+        })
+    }
+
+    Profile.updateOne({ username: req.params.username }, { firstName: req.body.firstName })
+        .then(() => {
+            res.status(200).json({
+                message: 'updated successfully'
+            });
+        })
+        .catch(error => {
+            res.status(400).json({
+                error: error
+            });
+        });
+});
+
+router.put('/updateLastName/:username', (req, res, next) => {
+    console.log("Hitting update last name endpt with id " + req.params.username)
+
+    // If lastName key is not in JSON body then return 400 status
+    if ((typeof req.body.lastName) === 'undefined') {
+        res.status(400).json({
+            error: error
+        })
+    }
+
+    Profile.updateOne({ username: req.params.username }, { lastName: req.body.lastName })
+        .then(() => {
+            res.status(200).json({
+                message: 'updated successfully'
+            });
+        })
+        .catch(error => {
+            res.status(400).json({
+                error: error
+            });
+        });
+});
+
+router.put('/updateDOB/:username', (req, res, next) => {
+    console.log("Hitting update DOB endpt with id " + req.params.username)
+
+    // If DOB key is not in JSON body then return 400 status
+    if ((typeof req.body.DOB) === 'undefined') {
+        res.status(400).json({
+            error: error
+        })
+    }
+
+    Profile.updateOne({ username: req.params.username }, { DOB: req.body.DOB })
+        .then(() => {
+            res.status(200).json({
+                message: 'updated successfully'
+            });
+        })
+        .catch(error => {
+            res.status(400).json({
+                error: error
+            });
+        });
+});
+
+router.put('/updatePicture/:username', (req, res, next) => {
+    console.log("Hitting update picture endpt with id " + req.params.username)
+
+    // If picture key is not in JSON body then return 400 status
+    if ((typeof req.body.picture) === 'undefined') {
+        res.status(400).json({
+            error: error
+        })
+    }
+
+    Profile.updateOne({ username: req.params.username }, { picture: req.body.picture })
+        .then(() => {
+            res.status(200).json({
+                message: 'updated successfully'
+            });
+        })
+        .catch(error => {
+            res.status(400).json({
+                error: error
+            });
+        });
+});
+
+router.put('/updateAbout/:username', (req, res, next) => {
+    console.log("Hitting update about endpt with id " + req.params.username)
+
+    // If about key is not in JSON body then return 400 status
+    if ((typeof req.body.about) === 'undefined') {
+        res.status(400).json({
+            error: error
+        })
+    }
+
+    Profile.updateOne({ username: req.params.username }, { about: req.body.about })
+        .then(() => {
+            res.status(200).json({
+                message: 'updated successfully'
+            });
+        })
+        .catch(error => {
+            res.status(400).json({
+                error: error
+            });
+        });
+});
+
+module.exports = router;
