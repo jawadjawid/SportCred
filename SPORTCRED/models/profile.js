8ee61cfe2322b032724752775cc115b25e06c25c
Jimmy Yu <jimmyyu82@gmail.com>
2020-10-19 18:28:57 -0400

    added regex for DOB in the profile schema

--- SPORTCRED/models/profile.js
+++ SPORTCRED/models/profile.js
@@ -16,7 +16,8 @@ const ProfileSchema = new Schema({
         type: String
     },
     DOB: {
-        type: String
+        type: String,
+        match: /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/
     },
     picture: {
         type: String
