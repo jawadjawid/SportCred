e0a8d72e3ce8147b88d9d96cae1db9f4d458e62c
Vineet <vineetdesai16@ymail.com>
2020-10-19 20:33:21 -0400

    Creates viewProfile file and adds some basic code about getting profile info.

---
+++ SPORTCRED/routes/api/viewProfile.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+
+const Profile = require('../../models/profile');
+
+router.get('/getUserProfile/:username', (req, res, next) => {
+    const givenUser = req.params.username;
+
+    Profile.find({username: givenUser})
+        .exec()
+        .then(userData => {
+            console.log(userData);
+
+            if(userData) {
+                res.status(200).json(userData);
+            } else {
+                res.status(404).json({
+                    message: 'username not in database'});
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({error: err});
+        });
+});
