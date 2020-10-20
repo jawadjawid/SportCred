8aaf253458dc29f1463368074a1c0b19e475136d
kshitij314 <kshitijdahal314@gmail.com>
2020-10-20 15:15:13 -0400

    change in backend connector for profile

--- SPORTCRED/client/src/backendConnector/profile.js
+++ SPORTCRED/client/src/backendConnector/profile.js
@@ -2,55 +2,69 @@ import axios from 'axios';
 // A function to send a POST request with the new user info
 export const getUserProfile = async (username, currPage) => {
 
-    // axios.get('http://localhost:5000/api/getUserProfile/' + username, currPage.state)
-    //     .then(res => {
-    //         if(res.status == 200) return res.data;
-    //     }).then(data => {
-    //         currPage.setState(data)
-    // });
-
-    currPage.setState({
-        userIcon:"https://material-ui.com/static/images/avatar/1.jpg",
-        friends: [{"fullName": "Abraham Lincoln", "username": "hello123"},
-            {"fullName": "John Doe", "username": "hi142"},
-            {"fullName": "Pussy Cat", "username": "meow","userIcon":"https://material-ui.com/static/images/avatar/2.jpg"},
-            {"fullName": "Albert Liu", "username": "alberto"},
-            {"fullName": "Mohammad Sajjad", "username": "mohao"},
-            {"fullName": "Abraham Lincoln", "username": "hello123","userIcon":"https://material-ui.com/static/images/avatar/3.jpg"},
-            {"fullName": "John Doe", "username": "hi142"},
-            {"fullName": "Pussy Cat", "username": "meow"},
-            {"fullName": "Albert Liu", "username": "alberto"},
-            {"fullName": "Mohammad Sajjad", "username": "mohao"}
-        ],
-        acsScore:"35",
-        acsHistoryReport:[{
-            "acsStart":"35",
-            "acsEnd":"41",
-            "activity":"Trivia with user5223 with final score of 142:42",
-            "date":"Oct 12"
-        },{
-            "acsStart":"31",
-            "acsEnd":"35",
-            "activity":"Debate won w post #4324",
-            "date":"Oct 7"
-        },{
-            "acsStart":"33",
-            "acsEnd":"31",
-            "activity":"Trivia w user3252 with final score of 100:24",
-            "date":"Oct 3"
-        }],
-        userBackground: [
-            {"Username":"bobby123"},
-            {"Full Name": "Bob Thisismylastnamehaha"},
-            {"Date of Birth":"02/03/2000"},
-            {"Email": "bobbybobbob@ilikeball.com"},
-            {"Favorite Sport": "Basketball"},
-            {"Favorite Player": "Jamal Jamal"},
-            {"Favorite Team": "Miami Heat"},
-            {"Odd Sport":"cricket"},
-            {"Highest Level of Sports Played": "college"}
-        ]
+    axios.get('http://localhost:5000/api/getUserProfile/' + username, currPage.state)
+        .then(res => {
+            if(res.status === 200) return res.data;
+        }).then(data => {
+            currPage.setState(
+                {
+                    userIcon:data.userIcon,
+                    friends:data.friends,
+                    acsScore:data.acsScore,
+                    acsHistoryReport:data.acsHistoryReport,
+                    userBackground: createUserBackground(data.username,data.fullName,data.dateOfBirth,data.email,data.userBackground)
+                }
+            );
+            currPage.setState(data)
     });
+
+    const createUserBackground = (username, fullName, dob, email, questionnaire) => {
+        // assuming questionnaire is an array
+        return [{"Username":username},{"Full Name":fullName},{"Date of Birth":dob},{"Email":email}].concat(questionnaire);
+    }
+
+    // currPage.setState({
+    //     userIcon:"https://material-ui.com/static/images/avatar/1.jpg",
+    //     friends: [{"fullName": "Abraham Lincoln", "username": "hello123"},
+    //         {"fullName": "John Doe", "username": "hi142"},
+    //         {"fullName": "Pussy Cat", "username": "meow","userIcon":"https://material-ui.com/static/images/avatar/2.jpg"},
+    //         {"fullName": "Albert Liu", "username": "alberto"},
+    //         {"fullName": "Mohammad Sajjad", "username": "mohao"},
+    //         {"fullName": "Abraham Lincoln", "username": "hello123","userIcon":"https://material-ui.com/static/images/avatar/3.jpg"},
+    //         {"fullName": "John Doe", "username": "hi142"},
+    //         {"fullName": "Pussy Cat", "username": "meow"},
+    //         {"fullName": "Albert Liu", "username": "alberto"},
+    //         {"fullName": "Mohammad Sajjad", "username": "mohao"}
+    //     ],
+    //     acsScore:"35",
+    //     acsHistoryReport:[{
+    //         "acsStart":"35",
+    //         "acsEnd":"41",
+    //         "activity":"Trivia with user5223 with final score of 142:42",
+    //         "date":"Oct 12"
+    //     },{
+    //         "acsStart":"31",
+    //         "acsEnd":"35",
+    //         "activity":"Debate won w post #4324",
+    //         "date":"Oct 7"
+    //     },{
+    //         "acsStart":"33",
+    //         "acsEnd":"31",
+    //         "activity":"Trivia w user3252 with final score of 100:24",
+    //         "date":"Oct 3"
+    //     }],
+    //     userBackground: [
+    //         {"Username":"bobby123"},
+    //         {"Full Name": "Bob Thisismylastnamehaha"},
+    //         {"Date of Birth":"02/03/2000"},
+    //         {"Email": "bobbybobbob@ilikeball.com"},
+    //         {"Favorite Sport": "Basketball"},
+    //         {"Favorite Player": "Jamal Jamal"},
+    //         {"Favorite Team": "Miami Heat"},
+    //         {"Odd Sport":"cricket"},
+    //         {"Highest Level of Sports Played": "college"}
+    //     ]
+    // });
 
 
 
@@ -58,12 +72,22 @@ export const getUserProfile = async (username, currPage) => {
 
 export const setUserProfile = async (username, profile) => {
 
-    // axios.post('http://localhost:5000/api/setUserProfile/' + username, profile)
-    //     .then(res => {
-    //         if(res.status === 200) return res.data;
-    //     }).then(data => {
-    //         currPage.setState(data)
-    // });
+    const dataToSet = {
+        username:profile.state.userBackground.username,
+        fullName:profile.state.userBackground.fullName,
+        dateOfBirth:profile.state.userBackground.dateOfBirth,
+        email:profile.state.userBackground.email,
+        userIcon:profile.state.userIcon,
+        acsScore:profile.state.acsScore,
+        acsHistoryReport: profile.state.acsHistoryReport,
+        friends:profile.state.friends
+    }
+
+    axios.post('http://localhost:5000/api/setUserProfile/' + username, dataToSet)
+        .then(res => {
+            if(res.status === 200) return res.data;
+        }).then(data => {
+    });
     console.log('setting user profile');
     console.log(username);
     console.log(profile);
