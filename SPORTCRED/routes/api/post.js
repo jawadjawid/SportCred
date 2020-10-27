04d55387a39505ec48b8676236c0da4dd5304843
Jimmy Yu <jimmyyu82@gmail.com>
2020-10-27 04:10:54 -0400

    [IL-41] Add routes to return all posts or posts created by a specified user

--- SPORTCRED/routes/api/post.js
+++ SPORTCRED/routes/api/post.js
@@ -32,7 +32,36 @@ router.post('/createPost/:username', (req, res) => {
                 });
             });
     });
+});
 
+router.post('/getAllPosts', (req, res) => {
+    Post.find({})
+        .sort({'postDate': 'desc'})
+        .exec()
+        .then(data => res.status(200).json(data))
+        .catch(error  => {
+            console.log(error);
+            res.status(500).json({error:error});
+        });
+});
+
+router.post('/getPosts/:username', (req, res) => {
+    Profile.findOne({username: req.params.username})
+    .exec(function(err, profile) {
+        if(profile == null) {
+            return res.status(400).json({message:"This user does not exist"})
+        }
+        else {
+            Post.find({poster: profile._id})
+                .sort({'postDate': 'desc'})
+                .exec()
+                .then(post => res.status(200).json(post))
+                .catch(error => {
+                    console.log(error);
+                    res.status(500).json({error:error});
+                });
+        }
+    });
 });
 
 module.exports = router;
