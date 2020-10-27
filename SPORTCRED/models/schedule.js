89f8d17d0cddb53be147e70971a8539b63df7e4d
Vineet <vineetdesai16@ymail.com>
2020-10-27 17:15:52 -0400

    [IL-35] Creates games schedule Schema and adds the necessary fields.

---
+++ SPORTCRED/models/schedule.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ScheduleSchema = new Schema({
+    teams: { type: String, required: true },
+    date: { type: String, 
+        match: /(\d{4})-(\d{2})-(\d{2})/, 
+        required: true },
+    time: { type: String, 
+        match: /((1[0-2]|0?[1-9]):([0-5][0-9]) ?([AaPp][Mm]))/,
+        required: true },
+    venue: { type: String, required: true}
+});
+
+module.exports = Schedule = mongoose.model('schedules', ScheduleSchema);
