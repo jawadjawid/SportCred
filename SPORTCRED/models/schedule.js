0d87d4256789c7ac15275366ad9522b73adfc07e
Vineet <vineetdesai16@ymail.com>
2020-10-27 20:42:33 -0400

    [IL-35] Adds match regex to teams field in the Schedule Schema.

--- SPORTCRED/models/schedule.js
+++ SPORTCRED/models/schedule.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ScheduleSchema = new Schema({
-    teams: { type: String, required: true },
+    teams: { type: String, 
+        match: /[a-zA-Z0-9\s-'()]+ [vs]+ [a-zA-Z0-9\s-'()]+/,
+        required: true },
     date: { type: String, 
         match: /(\d{4})-(\d{2})-(\d{2})/, 
         required: true },
