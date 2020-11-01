65f9d65182c968c2e719fcea379687c50a50116a
Jawad Jawid <jawadadham98@gmail.com>
2020-11-01 16:15:36 -0500

    [IL-32]: remove unnescasry comments

--- SPORTCRED/client/src/backendConnector/picksAndPredictions.js
+++ SPORTCRED/client/src/backendConnector/picksAndPredictions.js
@@ -1,22 +1,6 @@
 import axios from 'axios';
 // A function to send a get request with the match  info
 export const getUserPicksAndPredictions = async (currPage, val) => {
-    // if (currPage.state.currMonth.toString().length == 1) {
-    //     currPage.setState({
-    //         currMonth: '0' + currPage.state.currMonth.toString()
-    //     });
-    // }
-    //
-    // if (currPage.state.currDay.toString().length == 1) {
-    //     currPage.setState({
-    //         currDay: '0' + currPage.state.currDay.toString()
-    //     });
-    // }
-    //
-    // currPage.setState({
-    //     fullDate: currPage.state.currYear.toString() + '-' +currPage.state.currMonth.toString() + '-' +  currPage.state.currDay.toString()
-    // });
-
     axios.get('http://localhost:5000/api/schedule/upcomingMatch/today/' + val).then((res) => {
         if(res.status === 200) {
             return res.data;
