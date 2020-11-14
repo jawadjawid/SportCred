b07523228c011190985f0018deb525e13a1add07
yu-jimmy <50390471+yu-jimmy@users.noreply.github.com>
2020-11-14 14:55:27 -0500

    Yu jimmy/il 59 (#35)
    * [IL-59] Modify agreeance array
    
    * [IL-59] Add route that stores a users name and score value to a specific debate post
    
    * [IL-59] Add route to get total ACS score from a debate post
    
    * [IL-59] Add example route calls to both requests, as a comment

--- SPORTCRED/models/debatePost.js
+++ SPORTCRED/models/debatePost.js
@@ -5,7 +5,7 @@ const DebatePostSchema = new Schema({
     poster: {type: Schema.Types.ObjectId, ref: 'Profile'},
     postContent: { type: String, trim: true },
     postDate: {type: Date, default: Date.now},
-    agreeance: [ {type: String} ]
+    agreeance: [{agreer: {type: String}, score: {type: Number}}]
 });
 
 module.exports = DebatePost = mongoose.model('debatePost', DebatePostSchema);
--- SPORTCRED/routes/api/debate.js
+++ SPORTCRED/routes/api/debate.js
@@ -5,6 +5,8 @@ var cors = require('cors')
 const DebatePost = require('../../models/debatePost');
 const Debate = require('../../models/debate');
 const Profile = require('../../models/profile');
+const post = require('../../models/post');
+const { aggregate } = require('../../models/debatePost');
 
 var corsOptions = {
     origin: 'http://localhost:3000',
@@ -130,4 +132,73 @@ router.get('/getAllPostsByTier/:username', (req, res) => {
             }
         });
 });
+
+router.post('/updateAgreeOrDisagree/:username', (req, res) => {
+    /*
+    ...api/debate/updateAgreeOrDisagree/someUsername
+    {
+        "poster": "5faeec04c90b961834f6b21d",
+        "postContent": "Debate v good v nice",
+        "postDate": "2020-11-13T20:33:40.493+00:00",
+        "score": 60
+    }
+    */
+
+    Profile.findOne({username: req.params.username})
+        .exec(function(err, profile) {
+            if(profile == null) {
+                return res.status(400).json({message:"This user does not exist"})
+            } else {
+                DebatePost.findOne({poster: req.body.poster, postContent: req.body.postContent, postDate: req.body.postDate})
+                    .exec(function(err, post) {
+                        if(((typeof req.body.score) === 'undefined') || (req.body.score < 0)) {
+                            return res.status(400).json({message:"Bad request, score not given or is a negative number"})
+                        } else {
+                            let alreadyAgreed = false;
+                            let agreedIndex = -1;
+                            post.agreeance.forEach(function (item, index) {
+                                if(item.agreer === req.params.username) {
+                                    alreadyAgreed = true;
+                                    agreedIndex = index;
+                                }
+                            });
+                            if(alreadyAgreed && (agreedIndex > -1)) {
+                                post.agreeance.splice(agreedIndex, 1);
+                                post.agreeance.push({agreer: req.params.username, score: req.body.score})  
+                                post.save();
+                                res.status(200).json({message:"Updated successfully"})
+                                
+                            } else {
+                                post.agreeance.push({agreer: req.params.username, score: req.body.score})  
+                                post.save();
+                                res.status(200).json({message:"Updated successfully"})
+                            }
+                        }
+                    });
+            }
+        });
+});
+
+router.get('/getScoreFromPost', (req, res) => {
+    /*
+    {
+        "poster": "5faeec04c90b961834f6b21d",
+        "postContent": "Debate v good v nice",
+        "postDate": "2020-11-13T20:33:40.493+00:00"
+    } 
+    */
+    DebatePost.findOne({poster: req.body.poster, postContent: req.body.postContent, postDate: req.body.postDate})
+        .exec(function(err, post) {
+            if(post == null) {
+                return res.status(400).json({message: "This debate post does not exist"})
+            } else {
+                let totalASC = 0
+                post.agreeance.forEach(function(item, index) {
+                    totalASC += item.score;
+                });
+                res.status(200).json({ACSFromPost: totalASC})
+            }
+        });
+});
+
 module.exports = router;
